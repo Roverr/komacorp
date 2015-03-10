31baefbad81410489afe4548b60d24d4e4c4e236
kari <kari1038@gmail.com>
2015-03-10 11:48:25 +0100

    Eclipse Projekt
    Alap Eclipse porjekt elkészítve, ebbe lesz elkészítve a Szoftlab 4
    feladat. Mostantól mindenki írhat a dokumentációhoz megfelelő részeket.
    Egy ember inkább egy kisebb egységet vállaljon el egyszerre, ne itt ott
    javítson bele a kódba.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # komacorp
 School project at Budapest University of Technology and Economics
+
+If you want to commit to the code, please read the according part in the 'documentation' folder in order to meet our guidlines and rules of coding.
---
+++ src/Program/Program.java
@@ -0,0 +1,24 @@
+package Program;
+
+/**
+ * Az egész projekt belépési pontja. Ez készíti el a menüket, és játék indításakor a Game osztályt.
+ * 
+ * 
+ * @author Karesz
+ *
+ */
+
+public class Program {
+
+	/** main függvény, itt kreálódjon meg a menü majd a grafikus verzióhoz. 
+	 *  Egyelõre egy dummy Game kreálódjon.
+	 * 
+	 * @param args - Standard bemenet. 
+	 * @author Karesz
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// new Game(180 seconds, map:"Halálos kanyon");
+	}
+
+}
