5e0d9c076e9fc21a0696e03c61f8a97376875d7c
Hunor <nyekics@gmail.com>
2015-04-16 23:51:35 +0200

    PlayerRobot ugrása és sebességvektor módosítása

--- src/Program/Core/PlayerRobot.java
+++ src/Program/Core/PlayerRobot.java
@@ -85,9 +85,14 @@ public class PlayerRobot extends Robot implements Serializable  {
 		
 	}
 	
-	
+	/**
+	 * Sebességet módosítja (ami a következõ ugrást határozza meg)
+	 * A sebességvektorhoz hozzáadja a paraméterként kapott vektort
+	 * @param force
+	 * @author Hunor
+	 */
 	public void modifySpeed(Vector force) {
-		modSpeed = force;
+		modSpeed.add(force);
 	}
 	
 	public Vector getModSpeed(){
@@ -119,12 +124,34 @@ public class PlayerRobot extends Robot implements Serializable  {
 	
 	/**
 	 * A robot frissítõ függvénye
+	 * Elugráskor a pozíciója változik a robotnak, a módosítandó sebességvektor(modSpeed)
+	 * függvényében.
+	 * @author Hunor
 	 */
 	public void jump() {
 		SkeletonUtility.printCall("Jump", this);
-		/**
-		 * TODO 
-		 */
+		
+		/*TODO 
+		 * 1) Miért a Point-et használjuk a pozícióra?
+		 * Miért nem használjuk a Vector-t, ha már megírtuk? 
+		 * Pozíciót csak int pontosságra tárolunk? (Mert a point az int pontosságú, ezért
+		 * kell kasztolnom egy csomót, mert a vektorunk meg double)
+		 * Nem utolsó sorban hozzá se tudok adni a pozícióhoz vektort (nem ez lett
+		 * volna a modSpeed lényege?).
+		 * 2) Miért van a doksiban egy move függvény? Nem az ugráskor mozdul el a robot?*/
+		
+		/*Lekérdezem az aktuális pozíciót*/
+		int x = (int) this.getPosition().getX();
+		int y = (int) this.getPosition().getY();
+		
+		/*Meghatározom az új pozíciót (hozzáadódik a mostanihoz a sebességvektor)*/
+		x += modSpeed.getX();
+		y += modSpeed.getY();
+		Point newPosition = new Point(x, y);
+		
+		/*Módosul a pozíció*/
+		this.setPosition(newPosition);
+		
 		SkeletonUtility.printReturn("Jump", this);
 	}
 	
@@ -147,7 +174,6 @@ public class PlayerRobot extends Robot implements Serializable  {
 	public void setWantToDrop(int to) {
 		wantToDrop = to;
 	}
-	
 	
 	
 }
